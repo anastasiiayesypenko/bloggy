a754481dc659146cea5877954263560482ce623c
Anastasia <nastiyayesypenko@ukr.net>
2019-09-11 22:10:11 +0300

    Updates

---
+++ precache-manifest.fecc012e2121879c52969c338e2a8d3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b88c59471cc8a5b0f614012a7d56ebc2",
+    "url": "/bloggy/index.html"
+  },
+  {
+    "revision": "98bce1671dffdf20e583",
+    "url": "/bloggy/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "4086cf0ae34d7e4600f1",
+    "url": "/bloggy/static/css/main.43f4691e.chunk.css"
+  },
+  {
+    "revision": "98bce1671dffdf20e583",
+    "url": "/bloggy/static/js/2.787a28a8.chunk.js"
+  },
+  {
+    "revision": "4086cf0ae34d7e4600f1",
+    "url": "/bloggy/static/js/main.d6c0eead.chunk.js"
+  },
+  {
+    "revision": "df2641b8ba45c076d5f1",
+    "url": "/bloggy/static/js/runtime~main.e3b253fb.js"
+  }
+]);
